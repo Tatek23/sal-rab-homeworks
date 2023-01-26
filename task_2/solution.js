ae4bd5e73d451a35ac04be8d000882f98e9e56a4
Татек23 <belokonenko.stas@mail.ru>
2023-01-26 23:41:14 +0300

    Task 2 failed

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,14 +7,17 @@ function calcShipping(sum, min, shipping) {
 
    let shippingSum; // создайте переменную shippingSum
 
-   if (productsSum == '0') { // если productsSum равно 0,
-    shippingSum = 0};// то shippingSum присвоить значение 0
 
-    if (productsSum >= freeShippingMinSum) {// если productsSum Больше или равна freeShippingMinSum,
-    shippingSum = 0};// то shippingSum присвоить значение 0
-
-     if (productsSum > 0 && productsSum < freeShippingMinSum) {// если productsSum больше 0 и меньше freeShippingMinSum,
-    shippingSum = shippingPrice};// то shippingSum присвоить значение shippingPrice
+   
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+      } 
+      else shippingSum = shippingPrice
+    // если productsSum равно 0, то shippingSum присвоить значение 0
+    // если productsSum Больше или равна freeShippingMinSum,
+    // то shippingSum присвоить значение 0
+    // если productsSum больше 0 и меньше freeShippingMinSum,
+    // то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
 
@@ -30,9 +33,11 @@ function calcDiscount(sum, min, discount) {
 
    let discountSum; // создайте переменную discountSum
 
-   if (productsSum >= discountMinSum) { // если productsSum больше или равно discountMinSum,
-    discountSum = productsSum / 100 * discountPart;} // то присвойте discountSum значение discountPart процентов от productsSum,
-    else discountSum = 0;// иначе присвойте discountSum значение 0
+   let discountSum = productsSum >= discountMinSum ? productsSum / 100 * discountPart : 0
+   else discountSum = 0;
+    // если productsSum больше или равно discountMinSum,
+    // то присвойте discountSum значение discountPart процентов от productsSum,
+    // иначе присвойте discountSum значение 0
 
     // Конец решения задания №2.2.
 
@@ -56,7 +61,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
    let freeShipping // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
-    freeShipping = !Boolean(shippingSum); // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    freeShipping = !(shippingSum); // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
     // Конец решения задачи №2.3.
 
