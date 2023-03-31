26156a236cf1b18bb57d617f9cacc3ff0786ca0e
Tatek <belokonenko.stas@mail.ru>
2023-03-31 16:56:53 +0300

    Task ver 2

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,18 +1,18 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-        let data = JSON.parse(json);
-        return data.products;
-        }
-        
-        function renderProductsCards(json) {
-        clearProducts();
-        let products = parseProducts(json);
-        let length = products.length;
-        for (let i = 0; i < (length-1); i +=1) {
-        addProduct(products[i]);
-        }
-        }
+	let data = JSON.parse(json);
+	return data.products;
+}
+
+function renderProductsCards(json) {
+	clearProducts();
+	let products = parseProducts(json);
+	let length = products.length;
+	for (let i = 0; i < length; i += 1) {
+		addProduct(products[i]);
+	}
+}
 
 
     // Аргументом функции является JSON
